51e6cc5de439aa7c3e860a7503068d0ee2c2c06a
Chetanya2001 <mathurchetanya1@gmail.com>
2025-05-08 11:37:49 +0530

    uuid

--- models/Company.model.js
+++ models/Company.model.js
@@ -1,10 +1,23 @@
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("Company", {
-    name: DataTypes.STRING,
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const Company = sequelize.define("Company", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4, // Automatically generates UUIDv4
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
     db_name: DataTypes.STRING,
     db_host: DataTypes.STRING,
     db_user: DataTypes.STRING,
     db_password: DataTypes.STRING,
-    db_port: DataTypes.INTEGER,
+    db_port: DataTypes.STRING,
   });
+
+  return Company;
 };
