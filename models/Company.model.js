5cc912ec5b1e259b88328f8208b4390578cbb1c4
Chetanya2001 <mathurchetanya1@gmail.com>
2025-05-15 16:53:00 +0530

    new

--- models/Company.model.js
+++ models/Company.model.js
@@ -1,23 +1,46 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  const Company = sequelize.define("Company", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4, // Automatically generates UUIDv4
-      primaryKey: true,
+  const Company = sequelize.define(
+    "Company",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      db_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      db_host: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      db_user: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      db_password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      db_port: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: "3306",
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    db_name: DataTypes.STRING,
-    db_host: DataTypes.STRING,
-    db_user: DataTypes.STRING,
-    db_password: DataTypes.STRING,
-    db_port: DataTypes.STRING,
-  });
+    {
+      tableName: "Companies", // Ensure it matches your actual table name
+      timestamps: false, // Disable if your DB doesn't use createdAt/updatedAt
+    }
+  );
 
   return Company;
 };
