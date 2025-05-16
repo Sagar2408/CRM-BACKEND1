ffb55039a6d24412ef48f1816754471b3c31a63d
Chetanya2001 <mathurchetanya1@gmail.com>
2025-05-16 16:54:02 +0530

    new

--- models/Notification.model.js
+++ models/Notification.model.js
@@ -1,49 +1,43 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  const Notification = sequelize.define(
-    "Notification",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Users", // Referencing the Users table
-          key: "id",
-        },
-        onDelete: "CASCADE", // If the user is deleted, delete their notifications
-      },
-      targetRole: {
-        type: DataTypes.STRING,
-        allowNull: false, // 'admin' or 'executive'
-        defaultValue: "executive",
-      },
-      message: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      is_read: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false, // By default, set to unread
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW, // Automatically set creation date
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW, // Automatically set the update date
-      },
+  const Notification = sequelize.define("Notification", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-      tableName: "Notifications", // Specify the table name if needed
-    }
-  );
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Users", // Referencing the Users table
+        key: "id",
+      },
+      onDelete: "CASCADE", // If the user is deleted, delete their notifications
+    },
+    targetRole: {
+      type: DataTypes.STRING,
+      allowNull: false, // 'admin' or 'executive'
+      defaultValue: "executive",
+    },
+    message: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    is_read: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false, // By default, set to unread
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW, // Automatically set creation date
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW, // Automatically set the update date
+    },
+  });
 
   return Notification;
 };
