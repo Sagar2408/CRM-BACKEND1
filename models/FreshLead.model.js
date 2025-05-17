77de3df3e56609c83cdb38cc2f20537502d17116
Chetanya2001 <mathurchetanya1@gmail.com>
2025-05-17 13:55:53 +0530

    freeze table

--- models/FreshLead.model.js
+++ models/FreshLead.model.js
@@ -1,65 +1,72 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  const FreshLead = sequelize.define("FreshLead", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    leadId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "Leads",
-        key: "id",
+  const FreshLead = sequelize.define(
+    "FreshLead",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
       },
-      onDelete: "CASCADE",
+      leadId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Leads",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      followUpDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: true,
+      },
+      followUpStatus: {
+        type: DataTypes.ENUM(
+          "appointment",
+          "no response",
+          "interested",
+          "converted",
+          "not interested",
+          "close"
+        ),
+        allowNull: true,
+      },
+      isChecked: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      callStatus: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    phone: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    followUpDate: {
-      type: DataTypes.DATEONLY,
-      allowNull: true,
-    },
-    followUpStatus: {
-      type: DataTypes.ENUM(
-        "appointment",
-        "no response",
-        "interested",
-        "converted",
-        "not interested",
-        "close"
-      ),
-      allowNull: true,
-    },
-    isChecked: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    callStatus: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-  });
+    {
+      tableName: "FreshLeads",
+      freezeTableName: true,
+    }
+  );
 
   return FreshLead;
 };
