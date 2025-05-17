7dbc7e3cf759cacb404a325bd3061d268cd08449
Chetanya2001 <mathurchetanya1@gmail.com>
2025-05-17 15:14:31 +0530

    added

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.options("*", cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
