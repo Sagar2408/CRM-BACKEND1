df2ce99eaf63d4d073abaf3501b4e60fc8d42aaa
Chetanya2001 <mathurchetanya1@gmail.com>
2025-05-17 15:07:16 +0530

    auth master

--- routes/Company.routes.js
+++ routes/Company.routes.js
@@ -4,9 +4,10 @@ const {
   createCompany,
   getCompaniesForMasterUser,
 } = require("../controllers/Company.controller");
+const authMaster = require("./middleware/authMaster");
 
 // No auth or tenantResolver needed
 router.post("/create-company", createCompany);
-router.get("/master/companies", getCompaniesForMasterUser); // Token required
+router.get("/master/companies", authMaster(), getCompaniesForMasterUser); // Token required
 
 module.exports = router;
