67991e27a48ed6c81d44e3c908144f5a79a0e2e8
anmolkole <anmolkole44@gmail.com>
2025-06-10 15:02:33 +0530

    added logout api for Hr

--- routes/Hr.routes.js
+++ routes/Hr.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { signupHr, loginHr } = require("../controllers/Hr.controller");
+const { signupHr, loginHr, logoutHR } = require("../controllers/Hr.controller");
 
 router.post("/signup", signupHr);
 router.post("/login", loginHr);
+router.post("/logout", logoutHr);
 
 module.exports = router;
