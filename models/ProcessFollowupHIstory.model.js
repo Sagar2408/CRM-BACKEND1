472e43d7f3a78b48b3ba29b23fa34d9220646c0f
Chetanya Mathur <chetanyamathur@Chetanyas-MacBook-Air.local>
2025-06-19 12:12:16 +0530

    process person

---
+++ models/ProcessFollowupHIstory.model.js
@@ -0,0 +1,65 @@
+module.exports = (sequelize) => {
+  const ProcessFollowUpHistory = sequelize.define(
+    "ProcessFollowUpHistory",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      fresh_lead_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "freshleads",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      process_person_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      connect_via: {
+        type: DataTypes.ENUM("Call", "Email", "Call/Email"),
+        allowNull: false,
+      },
+      follow_up_type: {
+        type: DataTypes.ENUM(
+          "document collection",
+          "payment follow-up",
+          "visa filing",
+          "other"
+        ),
+        allowNull: false,
+      },
+      follow_up_date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+      },
+      follow_up_time: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
+      comments: {
+        type: DataTypes.TEXT,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      tableName: "processfollowuphistories",
+      freezeTableName: true,
+      timestamps: true,
+    }
+  );
+
+  return ProcessFollowUpHistory;
+};
