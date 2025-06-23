0613e5157e7e1a1d05a2e1e64e68b01387f46ae1
anmolkole <anmolkole44@gmail.com>
2025-06-23 15:38:22 +0530

    added controller and route to add multile comments on a single stage

--- routes/CustomerStages.routes.js
+++ routes/CustomerStages.routes.js
@@ -4,12 +4,20 @@ const {
   createCustomerStages,
   getCustomerStages,
   updateCustomerStages,
-  getCustomerStagesById
+  getCustomerStagesById,
+  addStageComment,
+  getStageComments,
 } = require("../controllers/CustomerStages.controller");
 
 router.post("/stages", createCustomerStages);
 router.get("/stages", getCustomerStages);
 router.put("/stages", updateCustomerStages);
 router.get("/customer-stages/:customerId", getCustomerStagesById);
+
+//To add multiple comments on a single stage
+router.post("/stage-comment/add", addStageComment);
+
+//To Fetch all the comments on a single stage
+router.get("/stage-comment/get", getStageComments);
 
 module.exports = router;
