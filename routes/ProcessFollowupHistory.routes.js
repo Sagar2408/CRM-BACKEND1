f08192130bc1f9faa4a979710f3175fe6f148a90
anmolkole <anmolkole44@gmail.com>
2025-06-24 16:50:13 +0530

    added meeting api for process person

--- routes/ProcessFollowupHistory.routes.js
+++ routes/ProcessFollowupHistory.routes.js
@@ -25,10 +25,10 @@ router.get("/process-followup", getAllProcessFollowups);
 //change the status to rejected in process person panel
 router.post("/process-followup/reject", moveToRejected);
 
-//create meeting for process person
-router.post("/process-followp/create-meeting", createMeetingForProcessPerson);
-
 //get meeting for process person
 router.get("/process-followup/get-meeting", getProcessPersonMeetings);
+
+//create meeting for process person
+router.post("/process-followp/create-meeting", createMeetingForProcessPerson);
 
 module.exports = router;
