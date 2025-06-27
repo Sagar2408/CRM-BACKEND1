0b0c7f3ce903c72bbadb75a5f908bb0c5081fb64
anmolkole <anmolkole44@gmail.com>
2025-06-27 15:44:17 +0530

    fixed cors

--- utils/corsOption.js
+++ utils/corsOption.js
@@ -1,14 +1,3 @@
-// const allowedOrigins = [
-//   "http://localhost:3000",
-//   "https://crm-frontend-atozeevisas.vercel.app",
-//   "https://crm-frontend-live.vercel.app",
-//   "https://crm-frontend-eta-olive.vercel.app",
-// ];
-
-// module.exports = {
-//   origin: allowedOrigins,
-//   credentials: true,
-// };
 const allowedOrigins = [
   "http://localhost:3000",
   "https://crm-frontend-atozeevisas.vercel.app",
@@ -16,21 +5,33 @@ const allowedOrigins = [
   "https://crm-frontend-eta-olive.vercel.app",
 ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+module.exports = {
+  origin: allowedOrigins,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "x-company-id", // if you're using this
-  ],
 };
 
-module.exports = corsOptions;
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://crm-frontend-atozeevisas.vercel.app",
+//   "https://crm-frontend-live.vercel.app",
+//   "https://crm-frontend-eta-olive.vercel.app",
+// ];
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: [
+//     "Content-Type",
+//     "Authorization",
+//     "x-company-id", // if you're using this
+//   ],
+// };
+
+// module.exports = corsOptions;
