dd483b80ea63fc3644cd985a2b25004d34e34ec1
Chetanya2001 <mathurchetanya1@gmail.com>
2025-07-16 15:39:40 +0530

    cors added

--- utils/corsOption.js
+++ utils/corsOption.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "https://crm-frontend-live.vercel.app",
   "https://crm-frontend-eta-olive.vercel.app",
   "http://43.204.111.78",
+  "http://ec2-43-204-111-78.ap-south-1.compute.amazonaws.com",
 ];
 
 module.exports = {
